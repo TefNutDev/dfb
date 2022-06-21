ce15bbedd5a9a3d016a87af1e6d4d35ed8b10fb9
TefNutDev <evanmeleady@icloud.com>
2022-06-21 21:21:01 +0200

    cmity

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   },[])
 
   return (
-    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+    <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[99]' : 'fixed w-full h-20 z-[100]'}>
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
           <Link href='/'>
         <Image src={structure} alt="/" width="100" height="60" />
