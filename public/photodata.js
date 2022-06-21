ef88c1cd6421afab9f9c462ca09a6176564ebb64
TefNutDev <evanmeleady@icloud.com>
2022-06-21 22:08:18 +0200

    commit

--- public/photodata.js
+++ public/photodata.js
@@ -3,71 +3,71 @@
 export const data = [
     {
         id: 1,
-        img: '../public/assets/work/work5.jpg'
+        img: '/work5.jpg'
     },
     {
         id: 2,
-        img: '../public/assets/work/work16.jpg'
+        img: '/work16.jpg'
     },
     {
         id: 3,
-        img: '../public/assets/work/work17.jpg'
+        img: '/work17.jpg'
     },
     {
         id: 4,
-        img: '../public/assets/work/work13.jpg'
+        img: '/work13.jpg'
     },
     {
         id: 5,
-        img: '../public/assets/work/work14.jpg'
+        img: '/work14.jpg'
     },
     {
         id: 6,
-        img: '../public/assets/work/work6.jpg'
+        img: '/work6.jpg'
     },
     {
         id: 7,
-        img: '../public/assets/work/work7.jpg'
+        img: '/work7.jpg'
     },
     {
         id: 8,
-        img: '../public/assets/work/work8.jpg'
+        img: '/work8.jpg'
     },
     {
         id: 9,
-        img: '../public/assets/work/work9.jpg'
+        img: '/work9.jpg'
     },
     {
         id: 10,
-        img: '../public/assets/work/work10.jpg'
+        img: '/work10.jpg'
     },
     {
         id: 11,
-        img: '../public/assets/work/work11.jpg'
+        img: '/work11.jpg'
     },
     {
         id: 12,
-        img: '../public/assets/work/work12.jpg'
+        img: '/work12.jpg'
     },
     {
         id: 13,
-        img: '../public/assets/work/work15.jpg'
+        img: '/work15.jpg'
     },
     {
         id: 14,
-        img: '../public/assets/work/work1.jpg'
+        img: '/work1.jpg'
     },
     {
         id: 15,
-        img: '../public/assets/work/work4.jpg'
+        img: '/work4.jpg'
     },
     {
         id: 16,
-        img: '../public/assets/work/work2.jpg'
+        img: '/work2.jpg'
     },
     {
         id: 17,
-        img: '../public/assets/work/work3.jpg'
+        img: '/work3.jpg'
     },
 
 ]
