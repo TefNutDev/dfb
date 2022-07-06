a6ad0ffd33bda8b9e9b48c3b6a74d597a7e6ce89
TefNutDev <evanmeleady@icloud.com>
2022-07-06 14:14:31 +0200

    cmit

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 sitemap ({
     baseUrl: 'dfbuildingservices.vercel.app',
-    pagesDirectory: path.resolve(__dirname, '../out/'),
-    targetDirectory: path.resolve(__dirname, '../out/'),
+    pagesDirectory: path.resolve(__dirname, '../public/'),
+    targetDirectory: path.resolve(__dirname, '../public/'),
     ignoredExtensions: ['js', 'json', 'jpeg', 'jpg', 'icon', 'xml', 'png'],
     ignoredPaths: ['404', 'favicon']
 });
