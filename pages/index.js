814c07a969da1d5b8df5af54b838856903ff596d
TefNutDev <evanmeleady@icloud.com>
2022-07-06 13:31:59 +0200

    commit google analytics added

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,16 @@ export default function Home() {
         <title>DF Building Services</title>
         <meta name="description" content="Highly skilled Builder, +30 years of experience, no job too big or too small!" />
         <link rel="icon" href="/favicon.ico" />
+        
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-233681715-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-233681715-1');
+</script>
       </Head>
         <Navbar />
         <Main />
